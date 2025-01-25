dec16654352d89a89c80a112c2d2c08b0f7c333e
Alisher <roziqovalisher177@gmail.com>
2025-01-25 19:09:17 +0500

    first commit

---
+++ js/async.js
@@ -0,0 +1,122 @@
+const api = 'http://localhost:4000/data'
+import getData from "./sync.js"
+
+async function get() {
+    try {
+        let { data } = await axios.get(api)
+        getData(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function deleteUSer(id) {
+    try {
+        await axios.delete(`${api}/${id}`)
+        get()
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function addUser(user) {
+    try {
+        await axios.post(api, user)
+        get()
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function editUser(idx, user) {
+    try {
+        await axios.put(`${api}/${idx}`, user)
+        get()
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function checkUser(e) {
+    let status2 = !e.status
+    let user = {
+        ...e,
+        status: status2
+    }
+    try {
+        await axios.put(`${api}/${e.id}`, user)
+        get()
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function getById(e) {
+    try {
+        let { data } = await axios.get(`${api}/${e.id}`)
+        let infoImg = document.querySelector('.infoImg')
+        let infoName = document.querySelector('.infoName')
+        let infoEmail = document.querySelector('.infoEmail')
+        let infoCity =document.querySelector('.infoCity')
+        let infoStatus =document.querySelector('.infoStatus')
+        infoImg.src = data.avatar;
+        infoName.innerHTML = data.name;
+        infoStatus.innerHTML = data.status;
+        infoEmail.innerHTML = data.email;
+        infoCity.innerHTML =data.city;
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function filterUSers(value) {
+    try {
+        if (value == 'Active') {
+            let { data } = await axios.get(`${api}?status=${true}`)
+            getData(data);
+        }
+        else if (value == 'Inactive') {
+            let { data } = await axios.get(`${api}?status=${false}`)
+            getData(data);
+        }
+        else if (value == 'All') {
+            get();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function filterCity(value) {
+    try {
+        if (value == 'Dushanbe') {
+            let { data } = await axios.get(`${api}?city=${"Dushanbe"}`)
+            getData(data);
+        }
+        else if (value == 'Bokhtar') {
+            let { data } = await axios.get(`${api}?city=${"Bokhtar"}`)
+            getData(data);
+        }
+        else if (value == 'Kulob') {
+            let { data } = await axios.get(`${api}?city=${"Kulob"}`)
+            getData(data);
+        }
+        else if (value == 'All') {
+            get();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function searchUser(search) {
+    try {
+            let { data } = await axios.get(`${api}?name=${search}`)
+            getData(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export {searchUser}
+export {filterCity}
+export { filterUSers }
+export { getById }
+export { checkUser }
+export { editUser }
+export { addUser }
+export { deleteUSer }
+export default get
---
+++ js/sync.js
@@ -0,0 +1,161 @@
+let box = document.querySelector('.box')
+let tbody = document.querySelector('.tbody')
+let btnModal = document.querySelector('.btnModal')
+let divBtn1 = document.querySelector('.divBtn1')
+let addModal = document.querySelector('.addModal')
+let addForm = document.querySelector('.addForm')
+let addBtn = document.querySelector('.btnAdd')
+let editModal = document.querySelector('.editModal')
+let editForm = document.querySelector('.editForm')
+let infoModal = document.querySelector('.infoModal')
+let searchForm = document.querySelector('.searchForm')
+let filter = document.querySelector('.filter')
+filter.innerHTML = '<option value="All">All</option><option value="Active">Active</option><option value="Inactive">Inactive</option>';
+let filterCty = document.querySelector('.filterCity')
+filterCty.innerHTML = '<option value="All">All</option><option value="Dushanbe">Dushanbe</option><option value="Bokhtar">Bokhtar</option><option value="Kulob">Kulob</option>';
+let idx = null
+
+
+import { deleteUSer } from "./async.js";
+import { addUser } from "./async.js";
+import { editUser } from "./async.js";
+import { checkUser } from "./async.js"
+import { getById } from "./async.js"
+import { searchUser } from "./async.js"
+import { filterUSers } from "./async.js"
+import { filterCity } from "./async.js"
+
+
+function getData(data) {
+    tbody.innerHTML = ''
+    data.forEach((e) => {
+        let tr = document.createElement('tr')
+        //name        
+        let trName = document.createElement('div')
+        let tdName = document.createElement('td');
+        let divImg = document.createElement('div');
+        let divName = document.createElement('div');
+        let profImg = document.createElement('img');
+        profImg.src = e.avatar;
+        let pName = document.createElement('p')
+        let pEmail = document.createElement('p')
+        pName.innerHTML = e.name;
+        pEmail.innerHTML = e.email;
+        divImg.append(profImg);
+        divName.append(pName, pEmail)
+        //city
+        let tdCity = document.createElement('td');
+        let pCity = document.createElement('p');
+        pCity.innerHTML = e.city;
+        //status
+        let tdStat = document.createElement('td');
+        let pStat = document.createElement('p');
+        pStat.innerHTML = e.status;
+        //phone
+        let tdPhone = document.createElement('td');
+        let pPhone = document.createElement('p');
+        pPhone.innerHTML = e.phone;
+        //action
+        let tdAction = document.createElement('td')
+        let btnImage = document.createElement('img');
+        btnImage.src = '/images/1.png'
+        btnImage.onclick = () => {
+            btnModal.showModal();
+            divBtn1.innerHTML = "";
+            divBtn1.append(btnDelete, btnEdit, btnCheck, btnInfo, btnClose);
+            btnClose.onclick = () => {
+                btnModal.close();
+            }
+        };
+        let btnDelete = document.createElement('button')
+        btnDelete.innerHTML = 'Delete';
+        btnDelete.onclick = () => {
+            deleteUSer(e.id)
+        }
+        let btnEdit = document.createElement('button')
+        btnEdit.innerHTML = 'Edit';
+        btnEdit.onclick = () => {
+            editModal.showModal();
+            editForm['editName'].value = e.name;
+            editForm['editEmail'].value = e.email;
+            editForm['editImage'].value = e.avatar;
+            editForm['editPhone'].value = e.phone;
+            idx = e.id
+            editForm.onsubmit = (e) => {
+                e.preventDefault()
+                let stat = document.querySelector('.Status')
+                let city2 = document.querySelector('.City')
+                let newUser = {
+                    name: editForm['editName'].value,
+                    email: editForm['editEmail'].value,
+                    avatar: editForm['editImage'].value,
+                    phone: editForm['editPhone'].value,
+                    status: stat.value,
+                    city: city2.value
+                }
+                editUser(idx, newUser)
+            }
+            let btnClose = document.querySelector('.btnClose2')
+            btnClose.onclick = () => {
+                editModal.close();
+            }
+
+        }
+        let btnCheck = document.createElement('button')
+        btnCheck.innerHTML = 'Check';
+        btnCheck.onclick = () => {
+            checkUser(e)
+        }
+        let btnInfo = document.createElement('button')
+        btnInfo.innerHTML = 'Info';
+        btnInfo.onclick = () => {
+            infoModal.showModal();
+            getById(e);
+            let btnClose = document.querySelector('.btnClose');
+            btnClose.onclick = () => {
+                infoModal.close();
+            };
+        };
+        
+        let btnClose = document.querySelector('.btnClose')
+        btnClose.onclick = () => {
+            infoModal.close()
+        }
+        tdPhone.append(pPhone)
+        tdStat.append(pStat)
+        tdCity.append(pCity)
+        trName.append(divImg, divName)
+        tdName.append(trName)
+        tdAction.append(btnImage)
+        tr.append(tdName, tdCity, tdStat, tdPhone, tdAction)
+        tbody.append(tr)
+    });
+}
+addBtn.onclick = () => {
+    addModal.showModal();
+    addForm.onsubmit = (e) => {
+        e.preventDefault();
+        let newUser = {
+            name: addForm['addName'].value,
+            status: false,
+        };
+        addUser(newUser);
+    };
+}
+filter.onchange = () => {
+    let value = filter.value
+    filterUSers(value)
+}
+filterCty.onchange = () => {
+    let value = filterCty.value
+    filterCity(value)
+    console.log(filterCty.value);
+}
+searchForm.onsubmit = (e) => {
+    e.preventDefault()
+    let search = searchForm['search'].value;
+    searchUser(search)
+}
+
+
+export default getData
